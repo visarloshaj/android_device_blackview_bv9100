2a66702de8bd54a6f08b159d3162589e46c7c25d
bengris32 <bengris32@protonmail.ch>
2021-02-15 13:28:53 +0000

    bv9100: disable gl backpressure

--- product_prop.mk
+++ product_prop.mk
@@ -39,7 +39,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.latch_unsignaled=1 \
-    debug.sf.enable_gl_backpressure=1 \
     debug.sf.early_phase_offset_ns=500000 \
     debug.sf.early_app_phase_offset_ns=500000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
