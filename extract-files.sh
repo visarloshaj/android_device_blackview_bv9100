f26bba644260e8c9754a1bdef2a733640481d24b
bengris32 <bengris32@protonmail.ch>
2021-03-11 14:31:50 +0000

    bv9100: extract-files: correct ril blob path Since we are using prebuilt kernel, we cant use vendor overlay, as overlayfs is not enabled in prebuilt kernel

--- extract-files.sh
+++ extract-files.sh
@@ -55,7 +55,7 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        vendor/lib64/libmtk-ril.so)
+        system/lib64/libmtk-ril.so)
             sed -i 's|AT+EAIC=2|AT+EAIC=3|g' "${2}"
             ;;
     esac
